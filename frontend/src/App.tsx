2be3c037a79a307952f688cf345e3f9208d923aa
DorTKOphir <dor.ophir5@gmail.com>
2025-03-12 19:41:49 +0200

    Feat(SignIn & SignUp): Create the ui of the sign in and sign up pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <div>Hello world</div>
-    </>
-  )
+    <Router>
+      <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
+        <h1 className="text-3xl font-bold text-center">Fan Zone</h1>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
