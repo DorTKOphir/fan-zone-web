4942e36f08f4832c82b01919ae0933fa78dd226c
itamarattali <itamarattali@gmail.com>
2025-03-13 19:30:09 +0200

    feat(auth-backend) implement auth backend using bcrypt and jwt

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import app from './app';
+
+const PORT = process.env.PORT ?? 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as mongoose.ConnectOptions)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  });
---
+++ backend/src/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'FanZone',
+      version: '1.0.0',
+      description: 'FanZone, Football related social network',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
