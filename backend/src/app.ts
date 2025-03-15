5a84985ca3836035e66e4588853416a15ad0f622
idan janach <idanjanach4455@gmail.com>
2025-03-15 23:48:37 +0200

    added real matches fetching in back

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import postRoutes from './routes/postRoutes';
 import commentRoutes from './routes/commentRoutes';
+import matchRoutes from './routes/matchRoutes';
 import { setupSwagger } from './swagger/swagger';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
-app.use('./api/comments', commentRoutes)
+app.use('./api/comments', commentRoutes);
+app.use('/api/matches', matchRoutes);
 setupSwagger(app);
 
 export default app;
