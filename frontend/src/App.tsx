4937da7c95e587de7bdbdd82eabba7c30ee06634
idan janach <idanjanach4455@gmail.com>
2025-03-16 23:22:25 +0200

    change route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import MatchList from "./components/ui/MatchList";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import MatchList from './components/ui/MatchList';
 
 function App() {
-  return (
-    <Router>
-      <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
-        <h1 className="text-3xl font-bold text-center">Fan Zone</h1>
-        <Routes>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/Match-list" element={<MatchList />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="flex flex-col items-center justify-center min-h-screen space-y-6">
+				<h1 className="text-3xl font-bold text-center">Fan Zone</h1>
+				<Routes>
+					<Route path="/" element={<MatchList />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
