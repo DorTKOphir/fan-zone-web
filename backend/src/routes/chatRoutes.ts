091a4cf8aa98f115a2ec00a22254bb4561211777
itamarattali <itamarattali@gmail.com>
2025-03-17 20:16:54 +0200

    fix(chat-backend) issue with request parameters in getChatHistory

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -5,7 +5,7 @@ import authMiddleware from "../middlewares/authMiddleware";
 const router = express.Router();
 
 router.post("/send", authMiddleware ,ChatController.sendMessage.bind(ChatController));
-router.get("/history/:user1/:user2", authMiddleware ,ChatController.getChatHistory.bind(ChatController));
+router.get("/history/:firstUserId/:secondUserId", authMiddleware ,ChatController.getChatHistory.bind(ChatController));
 router.get("/chats",authMiddleware, ChatController.getAllChatsByUserId.bind(ChatController));
 
 export default router;
