1488ffc570daae5d228d07d19ab90b9ea4661a90
idan janach <idanjanach4455@gmail.com>
2025-03-23 20:40:04 +0200

    remove unwanted logs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-console.log(`GOOGLE_OAUTH_CLIENT_ID ${import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}`);
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
-console.log(`API_BASE_URL ${import.meta.env.VITE_API_BASE_URL}`);
+
 const api = axios.create({
 	baseURL: apiUrl,
 	headers: { 'Content-Type': 'application/json' },
