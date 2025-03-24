a0ddc9b165f7bbfeae0c34d7e9033d4187c0d0ac
idan janach <idanjanach4455@gmail.com>
2025-03-24 15:36:13 +0200

    remove excessive confug

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
 
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+	await mongoose.connect(process.env.MONGO_URI);
 });
 
 afterAll(async () => {
-  await mongoose.connection.close();
+	await mongoose.connection.close();
 });
--- backend/src/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-// Load test environment variables
-dotenv.config({ path: '.env' }); // use .env.test if needed
-
-// Mock console errors to keep test output clean
-jest.spyOn(console, 'error').mockImplementation(() => {});
